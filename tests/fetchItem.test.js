2c0ed32e2172a0cf4f2a7b3028adc8ad93cec5ec
Leonardo Zanette <leonardobzanette@gmail.com>
2022-06-06 16:55:40 -0300

    Req 3 - Done

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,21 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+
+  it('teste se a função fetchProducts é uma função', () => {
+    expect(typeof fetchItem).toEqual('function');
+  });
+
+  it('teste se a função é chamada e retorna o objeto', async () => {
+    const response = await fetchItem('MLB1615760527');
+    expect.assertions(3);
+    expect(fetch).toHaveBeenCalled();
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/items/MLB1615760527');
+    await expect(response).toEqual(item);
+  });
+
+  it('teste se a função fetch é chamada', async () => {
+    expect.assertions(1);
+    expect(await fetchItem()).toEqual(new Error('You must provide an url'));
+  });
 });
