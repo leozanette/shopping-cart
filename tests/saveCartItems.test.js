ce24d84f526d366716551f3f7e5d948208c9a084
Leonardo Zanette <leonardobzanette@gmail.com>
2022-06-07 11:25:37 -0300

    Req 6 - Done

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -5,5 +5,9 @@ localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
   // implemente seus testes aqui
-  fail('Teste vazio');
+  it('teste se ao executar a funão saveCartItems com o argumento <ol><li>Item</li></ol> o método localStorage.setItem é chamado', () => {
+    const local = saveCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.setItem).toHaveBeenCalled();
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItems','<ol><li>Item</li></ol>');
+  });
 });
